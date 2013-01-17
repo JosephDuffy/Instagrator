28b0966e672019b28085652300765ddb724a6816
Joseph Duffy <JosephDuffy@me.com>
2013-01-17 22:29:41 +0000

    Better handling of synced settings

--- Google Chrome/install.js
+++ Google Chrome/install.js
@@ -1,10 +1,20 @@
 // Saves options to localStorage.
 var storage = chrome.storage.sync;
 
-function createDefault() {
-    // Clear all saved options
-    storage.clear(function() {
-        // Storage has been cleared, save the options!
-        storage.set({'searchSite': 'http://statigr.am/tag/', 'target': '_blank', 'sendData': true});
+function loadOptions() {
+    storage.get(null, function(reponse) {
+        var options = reponse;
+        console.log(options);
+        if (typeof options.searchSite == 'undefined') {
+            storage.set({'searchSite': 'http://statigr.am/tag/'});
+        }
+        if (typeof options.target == 'undefined') {
+            storage.set({'target': '_blank'});
+        }
+        if (typeof options.searchSite == 'undefined') {
+            storage.set({'sendData': true});
+        }
     });
 }
+
+document.addEventListener('DOMContentLoaded', loadOptions);
